25b9685516e76458916d52c9265ebd4cdef8e90a
Alex Seif <alex.seif@gmail.com>
2016-11-24 12:00:52 +0200

    Some visual edits

--- src/AdminBundle/Controller/DefaultController.php
+++ src/AdminBundle/Controller/DefaultController.php
@@ -3,15 +3,21 @@
 namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
-    {
-        return $this->render('AdminBundle:Default:index.html.twig');
-    }
+
+  /**
+   * Admin Index
+   *
+   * @Route("/", name="admin_index")
+   * @Method("GET")
+   */
+  public function indexAction()
+  {
+    return $this->render('AdminBundle:Default:index.html.twig');
+  }
+
 }
