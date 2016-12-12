67d8c83fcf97694b0fbb60af2494db37ff4da185
Alex Seif <alex.seif@gmail.com>
2016-12-12 08:51:46 +0200

    Google Images like Portfolio layout

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,20 +8,28 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        return $this->render('default/index.html.twig');
-    }
-    /**
-     * @Route("/portfolio", name="portfolio")
-     */
-    public function portfolioAction(Request $request)
-    {
-        return $this->render('default/portfolio.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
-    }
+
+  /**
+   * @Route("/", name="homepage")
+   */
+  public function indexAction(Request $request)
+  {
+//        return $this->render('default/index.html.twig');
+    $em = $this->getDoctrine()->getManager();
+
+    $portfolios = $em->getRepository('AppBundle:Portfolio')->findBy(
+        array(), array('position' => 'ASC'));
+    return $this->render('default/portfolio.html.twig', array(
+          'portfolios' => $portfolios
+    ));
+  }
+
+  /**
+   * @Route("/portfolio", name="portfolio")
+   */
+  public function portfolioAction(Request $request)
+  {
+    return $this->render('default/portfolio.html.twig');
+  }
+
 }
---
+++ web/js/portfolio.js
@@ -0,0 +1,24 @@
+/* 
+ * The following content was designed & implemented under AlexSeif.com
+ */
+$(function () {
+  var $cell = $('.image__cell');
+
+  $cell.find('.image--basic').click(function () {
+    var $thisCell = $(this).closest('.image__cell');
+
+    if ($thisCell.hasClass('is-collapsed')) {
+      $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed');
+      $thisCell.removeClass('is-collapsed').addClass('is-expanded');
+    } else {
+      $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+    }
+  });
+
+  $cell.find('.expand__close').click(function () {
+
+    var $thisCell = $(this).closest('.image__cell');
+
+    $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+  });
+});
