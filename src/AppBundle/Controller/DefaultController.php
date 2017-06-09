3208406797c105a2eb17e6aeeece5c2b20d8cf28
Alex Seif <alex.seif@gmail.com>
2017-06-09 18:15:47 +0200

    Init page for Symfony 2 secret generator

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,6 +16,7 @@ class DefaultController extends Controller
   {
     return $this->render('default/comingSoon.html.twig');
   }
+
   /**
    * @Route("/beta", name="homepage")
    */
@@ -36,6 +37,39 @@ class DefaultController extends Controller
     return $this->render('default/portfolio.html.twig', array(
           'portfolios' => $portfolios
     ));
+  }
+
+  /**
+   * @Route("/secret", name="secret")
+   */
+  public function secretAction(Request $request)
+  {
+    $secret = $this->random_str(40);
+    return $this->render('default/secret.html.twig', array(
+          'secret' => $secret
+    ));
+  }
+
+  /**
+   * Generate a random string, using a cryptographically secure
+   * pseudorandom number generator (random_int)
+   *
+   * For PHP 7, random_int is a PHP core function
+   * For PHP 5.x, depends on https://github.com/paragonie/random_compat
+   *
+   * @param int $length How many characters do we want?
+   * @param string $keyspace A string of all possible characters
+   * to select from
+   * @return string
+   */
+  function random_str($length, $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+  {
+    $str = '';
+    $max = mb_strlen($keyspace, '8bit') - 1;
+    for ($i = 0; $i < $length; ++$i) {
+      $str .= $keyspace[random_int(0, $max)];
+    }
+    return $str;
   }
 
 }
