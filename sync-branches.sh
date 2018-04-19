4bdb69320e9cde76a6abe7921e01ed919ffa9882
Alex Seif <alex.seif@gmail.com>
2018-04-19 19:45:56 +0200

    sync branches

---
+++ sync-branches.sh
@@ -0,0 +1,9 @@
+git checkout wip
+git merge master
+git push origin wip
+git checkout careers
+git merge master
+git push origin careers
+git checkout symfony_upgrade
+git merge master
+git push origin symfony_upgrade
