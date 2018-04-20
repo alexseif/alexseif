4ac800669c03d68103e66fb444f3a5c2839c0b74
Alex Seif <alex.seif@gmail.com>
2018-04-20 19:02:51 +0200

    Final jobs touches

--- src/AppBundle/Controller/JobsController.php
+++ src/AppBundle/Controller/JobsController.php
@@ -78,17 +78,17 @@ class JobsController extends Controller
     $this->jobs['php_developer']['image'] = "php_developer.png";
     $this->jobs['php_developer']['definition']['title'] = "PHP Developer";
     $this->jobs['php_developer']['definition']['text'] = "As a  PHP developer your  responsibilities are writing server-side web applications logic, develop back-end components, connect the application with the other (often third-party) web services, and support the front-end developers by integrating their work with the application. Furthermore you often develop and integrate plugins for certain popular frameworks.";
-    $this->jobs['php_developer']['description'] = "We are looking for a PHP Developer responsible for managing back-end services and the interchange of data between the server and the users. You will be developing all server-side logic, defining and maintaining the central database, and ensuring high performance and responsiveness to requests from the front-end, as well as  integrating the front-end elements. Therefore, a good knowledge of front-end technologies is a must.";
+    $this->jobs['php_developer']['description'] = "We are looking for PHP Developers, mainly responsible for managing back-end services and the interchange of data between the server and the clients. You will be developing all server-side logic, defining and maintaining the central database, and ensuring high performance and responsiveness to requests from the front-end, as well as integrating the front-end elements. Therefore, a good knowledge of front-end technologies is required.";
     $this->jobs['php_developer']['responsibilities'][] = "Integration of user-facing elements developed by front-end developers";
     $this->jobs['php_developer']['responsibilities'][] = "Build efficient, testable, and reusable PHP modules";
     $this->jobs['php_developer']['responsibilities'][] = "Solve complex performance problems and architectural challenges";
     $this->jobs['php_developer']['responsibilities'][] = "Integration of data storage solutions";
-    $this->jobs['php_developer']['skills'][] = "Knowledge of PHP web frameworks such as Symfony";
+    $this->jobs['php_developer']['skills'][] = "Knowledge of PHP web frameworks such as Symfony ";
     $this->jobs['php_developer']['skills'][] = "Understanding the fully synchronous behavior of PHP";
     $this->jobs['php_developer']['skills'][] = "Understanding of MVC design patterns";
     $this->jobs['php_developer']['skills'][] = "Basic understanding of front-end technologies, such as JavaScript, HTML5, and CSS3";
     $this->jobs['php_developer']['skills'][] = "Knowledge of object oriented PHP programming";
-    $this->jobs['php_developer']['skills'][] = "Understanding accessibility and security compliance";
+    $this->jobs['php_developer']['skills'][] = "Understanding accessibility and security compliance ";
     $this->jobs['php_developer']['skills'][] = "Strong knowledge of the common PHP or web server exploits and their solutions";
     $this->jobs['php_developer']['skills'][] = "Understanding fundamental design principles behind a scalable application";
     $this->jobs['php_developer']['skills'][] = "User authentication and authorization between multiple systems, servers, and environments";
@@ -96,6 +96,7 @@ class JobsController extends Controller
     $this->jobs['php_developer']['skills'][] = "Familiarity with limitations of PHP as a platform and its workarounds";
     $this->jobs['php_developer']['skills'][] = "Creating database schemas that represent and support business processes";
     $this->jobs['php_developer']['skills'][] = "Familiarity with SQL/NoSQL databases and their declarative query languages";
+    $this->jobs['php_developer']['skills'][] = "Ability to deliver clean code is a must";
     $this->jobs['php_developer']['skills'][] = "Proficient understanding of code versioning tools, such as Git";
     $this->jobs['php_developer']['skills'][] = "Comfortable in speaking, reading and writing in Arabic & English";
   }
@@ -106,8 +107,9 @@ class JobsController extends Controller
     $this->jobs['wordpress_developer']['link'] = $this->generateUrl('wordpress_developer');
     $this->jobs['wordpress_developer']['image'] = "wordpress_developer.png";
     $this->jobs['wordpress_developer']['definition']['title'] = "WordPress";
-    $this->jobs['wordpress_developer']['definition']['text'] = "WordPress is one of the most powerful open source blogging and content management systems available online today. Clients love it because it’s easy to set up, there are a variety of plugins, and it has a user-friendly approach. Developers like it because it has understandable functionality, which means it’s easy enough to set up yet still gives decent quality. As a result, the number of sites using WordPress is growing, and it’s becoming more important than ever to be able to separate great WordPress developers from the rest of the pack.";
-    $this->jobs['wordpress_developer']['description'] = "We are seeking a WordPress Developer responsible for both back-end and front-end development, including creating WordPress themes and plugins, This position requires a combination of programming skills (such as PHP, HTML5, CSS3, JavaScript) and aesthetics (understanding element arrangements on the screen, the color and font choices, etc.). Having a profound knowledge of the industry trends and content management systems and good experience with the responsive and adaptive design, in addition to an understanding of the entire web development process, including design, development, and deployment is preferred.";
+    $this->jobs['wordpress_developer']['definition']['text'] = "We are seeking WordPress Developers responsible for both back-end and front-end development, including creating WordPress themes and plugins, This position requires a combination of programming skills (such as PHP, HTML5, CSS3, JavaScript) and aesthetics (understanding element arrangements on the screen, the color and font choices, etc.). 
+Having knowledge of the industry trends and content management systems and good experience with the responsive and adaptive design, in addition to an understanding of the entire web development process, including design, development, and deployment is preferred.";
+    $this->jobs['wordpress_developer']['description'] = "We are seeking a WordPress Developer responsible for both back-end and front-end development, including creating WordPress themes and plugins, This position requires a combination of programming skills (such as PHP, HTML5, CSS3, JavaScript) and aesthetics (understanding element arrangements on the screen, the color and font choices, etc.). Having knowledge of the industry trends and content management systems and good experience with the responsive and adaptive design, in addition to an understanding of the entire web development process, including design, development, and deployment is preferred.";
     $this->jobs['wordpress_developer']['responsibilities'][] = "Designing and implementing new features and functionality";
     $this->jobs['wordpress_developer']['responsibilities'][] = "Establishing and guiding the website’s architecture";
     $this->jobs['wordpress_developer']['responsibilities'][] = "Ensuring high-performance and availability, and managing all technical aspects of the CMS";
@@ -120,6 +122,7 @@ class JobsController extends Controller
     $this->jobs['wordpress_developer']['skills'][] = "Ability to convert comprehensive layout and wireframes into working HTML pages";
     $this->jobs['wordpress_developer']['skills'][] = "Knowledge of how to interact with RESTful APIs and formats (JSON, XML)";
     $this->jobs['wordpress_developer']['skills'][] = "Strong understanding of PHP back-end development";
+    $this->jobs['wordpress_developer']['skills'][] = "Ability to deliver clean code is a must";
     $this->jobs['wordpress_developer']['skills'][] = "Proficient understanding of code versioning tools, such as Git";
     $this->jobs['wordpress_developer']['skills'][] = "Comfortable in speaking, reading and writing in Arabic & English";
   }
@@ -131,7 +134,8 @@ class JobsController extends Controller
     $this->jobs['drupal_developer']['image'] = "drupal_developer.png";
     $this->jobs['drupal_developer']['definition']['title'] = "Drupal";
     $this->jobs['drupal_developer']['definition']['text'] = "Drupal is internationally used by a vast and diverse community, building and creating a strong open source content management platform prompting millions of websites and applications worldwide.";
-    $this->jobs['drupal_developer']['description'] = "We are looking for a Drupal Developer responsible for front-end development, theming and back-end/module development, combining   programming skills (PHP, HTML, CSS, JS, TWIG etc.) and aesthetics (understanding element arrangements on the screen, the color and font choices, etc. ). Having a profound knowledge of the industry trends, content management systems and  experience with responsive and adaptive design, in addition to an understanding of the entire web development process, including design, development and deployment knowledge, is preferred.";
+    $this->jobs['drupal_developer']['description'] = "We are looking for Drupal Developers responsible for front-end development, theming and back-end/module development, combining programming skills (PHP, JS, mySQL, HTML, CSS, TWIG etc.) and aesthetics (understanding element arrangements on the screen, the color and font choices, etc. ). 
+Having knowledge of the industry trends, content management systems and  experience with responsive and adaptive design, in addition to an understanding of the entire web development process, including design, development and deployment knowledge, is preferred.";
     $this->jobs['drupal_developer']['responsibilities'][] = "Responsible for helping formulate an effective, responsive design and turning it into a working theme. Work closely with back-end developers and customers to ensure an effective, visually appealing, and intuitive implementation.";
     $this->jobs['drupal_developer']['responsibilities'][] = "Responsible for designing and implementing new features and functionality, establishing and guiding the website’s architecture, ensuring high-performance and availability, and managing all technical aspects of the CMS. Work closely with front-end developers and customers to ensure an effective, visually appealing, and intuitive implementation.";
     $this->jobs['drupal_developer']['skills'][] = "Experience building user interfaces for websites and/or web applications";
@@ -148,7 +152,8 @@ class JobsController extends Controller
     $this->jobs['drupal_developer']['skills'][] = "Experience creating custom Drupal modules";
     $this->jobs['drupal_developer']['skills'][] = "Experience building responsive websites using Drupal CMS, HTML/HTML5, CSS/CSS3, and JavaScript/jQuery";
     $this->jobs['drupal_developer']['skills'][] = "Knowledge of PHP, PHP files and theme functions, and knowledge of the Drupal theme layer";
-    $this->jobs['drupal_developer']['skills'][] = "Through understanding of the Software Development Lifecycle (e.g. Requirements, Design, Development, Testing) and exposure to Agile or iterative SDLCs";
+    $this->jobs['drupal_developer']['skills'][] = "Through understanding of the Software Development Lifecycle (e.g. Requirements, Design, Development, Testing)";
+    $this->jobs['drupal_developer']['skills'][] = "Ability to deliver clean code is a must";
     $this->jobs['drupal_developer']['skills'][] = "Proficient understanding of code versioning tools, such as Git";
     $this->jobs['drupal_developer']['skills'][] = "Comfortable in speaking, reading and writing in Arabic & English";
   }
