c86c93079b0bd0acec19b0fc94f8f8d0a6e42718
Alex Seif <alex.seif@gmail.com>
2019-07-20 18:00:32 +0200

    fix deployer

--- deployer.sh
+++ deployer.sh
@@ -1,7 +1,7 @@
 export PATH="$PATH:/usr/bin"
 git pull
 export SYMFONY_ENV=prod
-php-latest -d allow_url_fopen=1 -d php_openssl=1 composer.phar install --no-dev --optimize-autoloader 
+php-latest -d allow_url_fopen=1 -d php_openssl=1 -d open_basedir=NULL composer.phar install --no-dev --optimize-autoloader 
 php-latest app/console cache:clear --env=prod --no-debug
 php-latest app/console doctrine:migrations:migrate --no-debug --no-interaction --env=prod 
 php-latest app/console assets:install --env=prod 
