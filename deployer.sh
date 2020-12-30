df0365ed8e16187004b79545b90fd8927a48e10b
Alex Seif <alex.seif@gmail.com>
2020-12-30 20:18:28 +0200

    Deployers

--- deployer.sh
+++ deployer.sh
@@ -2,6 +2,6 @@ export PATH="$PATH:/usr/bin"
 git pull
 export SYMFONY_ENV=prod
 php-latest -d allow_url_fopen=1 -d php_openssl=1 -d open_basedir=NULL composer.phar install --no-dev --optimize-autoloader 
-php-latest app/console cache:clear --env=prod --no-debug
-php-latest app/console doctrine:migrations:migrate --no-debug --no-interaction --env=prod 
-php-latest app/console assets:install --env=prod 
+php-latest bin/console cache:clear --env=prod --no-debug
+php-latest bin/console doctrine:migrations:migrate --no-debug --no-interaction --env=prod 
+php-latest bin/console assets:install --env=prod 
