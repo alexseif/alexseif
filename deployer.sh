d323e4e9d46cf897cf25bd6dbbf9e7a5cc40edf9
Alex Seif <alex.seif@gmail.com>
2023-03-26 11:25:55 +0200

    do deployer

--- deployer.sh
+++ deployer.sh
@@ -1,7 +1,6 @@
-export PATH="$PATH:/usr/bin"
 git pull
 export SYMFONY_ENV=prod
-php-latest -d allow_url_fopen=1 -d php_openssl=1 -d open_basedir=NULL composer.phar install --no-dev --optimize-autoloader 
-php-latest bin/console cache:clear --env=prod --no-debug
-php-latest bin/console doctrine:migrations:migrate --no-debug --no-interaction --env=prod 
-php-latest bin/console assets:install --env=prod 
+composer install --no-dev --optimize-autoloader
+php7.4 bin/console cache:clear --env=prod --no-debug
+php7.4 bin/console doctrine:migrations:migrate --no-debug --no-interaction --env=prod
+php7.4 bin/console assets:install --env=prod
