e81fe1872ea5c37885420f6b7e6abdf4ae152e3c
Alex Seif <alex.seif@gmail.com>
2023-12-28 06:17:58 +0200

    attempting to fix images

--- webpack.config.js
+++ webpack.config.js
@@ -88,7 +88,7 @@ Encore
     .copyFiles({
         from: './assets/images',
         //optional target path, relative to the output dir
-        to: '/images/[path][name].[ext]',
+        to: 'images/[path][name].[ext]',
 
         //if versioning is enabled, add the file hash too
         // to: 'images/[path][name].[hash:8].[ext]',
