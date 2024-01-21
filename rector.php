55ff73a2577fb9cac04616a479849dfbd3e23c64
Alex Seif <alex.seif@gmail.com>
2024-01-21 11:28:48 +0200

    rector upgrades symfony/ux-chartjs

---
+++ rector.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Symfony\Set\SensiolabsSetList;
+use Rector\Symfony\Set\SymfonySetList;
+
+return function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+      __DIR__ . '/src',
+    ]);
+    $rectorConfig->sets([
+      DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+      SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+    ]);
+};
