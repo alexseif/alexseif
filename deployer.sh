c2b90612ec1e3735225c5d23622d711e619bd1b1
Alex Seif <alex.seif@gmail.com>
2024-07-24 19:32:18 +0300

    deployer update for npm

--- deployer.sh
+++ deployer.sh
@@ -5,5 +5,5 @@ php8.2 /usr/local/bin/composer dump-env prod
 php8.2 /usr/local/bin/composer install --no-dev --optimize-autoloader
 APP_ENV=prod APP_DEBUG=0 php8.2 bin/console cache:clear --env=prod
 php8.2 bin/console doctrine:migrations:migrate --no-interaction --env=prod
-yarn install
-./node_modules/.bin/encore production
+npm install
+npm run build
