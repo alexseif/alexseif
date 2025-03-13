9aa69de7c259961b7944776f536e551f481e55ce
Alex Seif <alex.seif@gmail.com>
2025-03-13 11:11:24 +0200

    build: local deployer

---
+++ deployer-local.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git reset HEAD --hard
+git pull origin master
+php8.2 /usr/local/bin/composer dump-env dev
+php8.2 /usr/local/bin/composer install --optimize-autoloader
+APP_ENV=prod APP_DEBUG=0 php8.2 bin/console cache:clear --env=dev
+php8.2 bin/console doctrine:migrations:migrate --no-interaction --env=dev
+npm install
+npm run build
