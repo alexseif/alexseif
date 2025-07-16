09b03f654e445e8b833a010f92191550149bb060
Alex Seif <alex.seif@gmail.com>
2025-07-16 18:22:50 +0300

    landing_page -> wordpress_optimize

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class LandingPageController extends AbstractController
 {
-    #[Route('/landing_page', name: 'app_landing_page')]
+    #[Route('/wordpress_optimize', name: 'app_landing_page')]
     public function index(): Response
     {
         return $this->render('landing_page/index.html.twig', [
