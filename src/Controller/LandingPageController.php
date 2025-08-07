6d46ce6886223c6f801c99bf7ef8ff862b9853f5
Alex Seif <alex.seif@gmail.com>
2025-08-07 20:22:28 +0300

    chore: localizing landing page

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Finder\Finder;
@@ -16,8 +17,10 @@ class LandingPageController extends AbstractController
     }
 
     #[Route('/wordpress_optimize', name: 'app_landing_page')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $locale = $request->get('_locale', 'en');
+        
         // Scan portfolio directory for images
         $portfolioImages = [];
         $portfolioDir = $this->getParameter('kernel.project_dir') . '/assets/images/portfolio';
@@ -31,7 +34,39 @@ class LandingPageController extends AbstractController
             }
         }
 
+        // Use main template for all locales, translations will handle language
         return $this->render('landing_page/index.html.twig', [
+            'controller_name' => 'LandingPageController',
+            'portfolio_images' => $portfolioImages,
+        ]);
+    }
+
+    #[Route('/wordpress_optimize/{_locale}', name: 'app_landing_page_locale')]
+    public function indexLocale(Request $request, string $_locale): Response
+    {
+        // Validate locale
+        if (!in_array($_locale, ['en', 'ar', 'eg'])) {
+            return $this->redirectToRoute('app_landing_page', ['_locale' => 'en']);
+        }
+
+        // Scan portfolio directory for images
+        $portfolioImages = [];
+        $portfolioDir = $this->getParameter('kernel.project_dir') . '/assets/images/portfolio';
+
+        if (is_dir($portfolioDir)) {
+            $finder = new Finder();
+            $finder->files()->in($portfolioDir)->name(['*.jpg', '*.jpeg', '*.png', '*.gif', '*.svg', '*.webp']);
+
+            foreach ($finder as $file) {
+                $portfolioImages[] = $file->getFilename();
+            }
+        }
+        $page = 'landing_page/index';
+if($_locale != 'en'){
+$page .= '_'.$_locale;
+}
+$page .= '.html.twig';
+        return $this->render($page, [
             'controller_name' => 'LandingPageController',
             'portfolio_images' => $portfolioImages,
         ]);
