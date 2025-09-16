86562547ee9bad55f145c5aaa4dbd8d77a19e43f
Alex Seif <alex.seif@gmail.com>
2025-09-16 17:29:14 +0300

    chore: webp  conversion and js light load

--- assets/app.js
+++ assets/app.js
@@ -8,13 +8,8 @@
 
 // start the Stimulus application
 // import './bootstrap';
-// import 'html5shiv';
 // any CSS you import will output into a single css file (app.css in this case)
 import './scss/app.scss';
-// import $ from 'jquery';
-// require('bootstrap');
-import 'fittext.js';
-// import fullpage from 'free-fullpage';
 
 /**
  * The following content was designed & implemented under AlexSeif.com
@@ -23,39 +18,12 @@ import 'fittext.js';
  * Function to resize text to fill its container
  * @param {HTMLElement} element - The element containing the text
  */
-function resizeTextToFill(element) {
-    if (element) {
-        const container = element.parentElement;
-        let fontSize = parseInt(window.getComputedStyle(element).fontSize, 10);
 
-        // Increase font size until it fills the container
-        while (element.scrollWidth < (container.clientWidth - 20) && element.scrollHeight < container.clientHeight && element.scrollHeight < (window.innerHeight - 400)) {
-            fontSize++;
-            element.style.fontSize = fontSize + 'px';
-        }
-
-        // Optionally, decrease font size if it exceeds the container
-        while (element.scrollWidth > container.clientWidth || element.scrollHeight > container.clientHeight) {
-            fontSize--;
-            element.style.fontSize = fontSize + 'px';
-        }
-    }
-}
 
 
 $(function () {
-    const textElement = $(".bigtext"); // Adjust selector as needed
-    resizeTextToFill(textElement[0]); // Call the function with the first matched element
 
-    //on window resize fire resizeTextToFill
-    $(window).on('resize', function () {
-        resizeTextToFill(textElement[0]); // Call the function with the first matched
-    });
-    $(".x-menu-btn").on("click", function () {
-        $(this).toggleClass("x-open");
-        $("#nav-container").toggleClass("x-open");
-        $('body').toggleClass('menu-open');
-    });
+
 
     // assets/js/Nav.js
 
