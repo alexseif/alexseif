29364d12ac6ec3358fa377e0a60009db17a2207c
Alex Seif <alex.seif@gmail.com>
2025-11-18 11:10:06 +0200

    chore: create user cli

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-user',
+    description: 'Creates a new user in the database',
+)]
+class CreateUserCommand extends Command
+{
+    private EntityManagerInterface $em;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher)
+    {
+        parent::__construct();
+        $this->em = $em;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'The email of the user')
+            ->addArgument('password', InputArgument::REQUIRED, 'The plain password')
+            ->addArgument('role', InputArgument::OPTIONAL, 'Role (default ROLE_ADMIN)', 'ROLE_ADMIN');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $email = $input->getArgument('email');
+        $plainPassword = $input->getArgument('password');
+        $role = $input->getArgument('role');
+
+        $user = new User();
+        $user->setEmail($email);
+        $user->setRoles([$role]);
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
+        $user->setPassword($hashedPassword);
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $output->writeln("<info>User $email created with role $role</info>");
+
+        return Command::SUCCESS;
+    }
+}
