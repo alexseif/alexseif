2ed132df08a36e482d0f9f39b169deece0fb7c2a
Alex Seif <alex.seif@gmail.com>
2026-01-14 08:45:00 +0200

    chore: experiments Yin Yang

--- src/Controller/ExperimentsController.php
+++ src/Controller/ExperimentsController.php
@@ -8,17 +8,27 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ExperimentsController extends AbstractController
 {
-    #[Route('/experiments/index', name: 'app_experiments')]
+    #[Route('/experiments/', name: 'app_experiments_index')]
     public function index(): Response
     {
-        return $this->render('experiments/index.html.twig', [
-            'controller_name' => 'ExperimentsController',
-        ]);
+        return $this->render('experiments/index.html.twig');
     }
 
-    #[Route('/experiments/graph', name: 'app_experiments')]
+    #[Route('/experiments/cta', name: 'app_experiments_cta')]
+    public function cta(): Response
+    {
+        return $this->render('experiments/cta.html.twig');
+    }
+
+    #[Route('/experiments/yinYang', name: 'app_experiments_yinYang')]
+    public function yinYang(): Response
+    {
+        return $this->render('experiments/yinYang.html.twig');
+    }
+
+    #[Route('/experiments/graph', name: 'app_experiments_graph')]
     public function graph(): Response
     {
-        return $this->render('experiments/graph.html.twig', []);
+        return $this->render('experiments/graph.html.twig');
     }
 }
